9055e7dfa6f7f6fb44e231051f8de8359a093fb1
omieli <mieli.ohad@gmail.com>
2020-09-21 12:54:40 +0300

    Ex1-5

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Ex1 - O(n)
+ * Ex2 - O(log(n)) - i jumps in loop with multiple, like binary search
+ * Ex3 - O(4) => O(1)
+ * Ex4 - studentGithubs.forEach => O(n)
+ *       Object.keys(answers).forEach => O(m) - different source => together O(mn)
+ * Ex5 - O(n) => number of recepients
+ *    
+ */
