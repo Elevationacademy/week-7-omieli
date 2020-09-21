9aec5aeee1536327288029765257f73f25b24769
omieli <mieli.ohad@gmail.com>
2020-09-21 14:33:12 +0300

    algo - ex1-3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/**
+/** Big O - Time Complexity (JS)
  * Ex1 - O(n)
  * Ex2 - O(log(n)) - i jumps in loop with multiple, like binary search
  * Ex3 - O(4) => O(1)
@@ -7,3 +7,34 @@
  * Ex5 - O(n) => number of recepients
  *    
  */
+
+
+/*
+ // Algorithms (JS)  
+  
+//Ex1
+const printStars = function (num) {
+    for (let i = 1; i <= num; i++){
+       console.log('*'.repeat(i))
+    }
+}
+
+//Ex2
+const printBackwardsStars = function (num1) {
+    for (let j = num1; j > 0; j--){
+       console.log('*'.repeat(j))
+    }
+}
+
+
+
+//Ex3
+const printStarSeries = function (num2, count) {
+    for (let k = 0; k < count; k++){
+        printStars(num2)
+        printBackwardsStars(num2-1)
+    }    
+}
+
+printStarSeries(5, 3)
+*/
