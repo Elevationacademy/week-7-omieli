95a5ce250622011559d0c0ac5821877d2c238a0a
omieli <mieli.ohad@gmail.com>
2020-09-22 09:26:15 +0300

    without console.log().repeat

--- main.js
+++ main.js
@@ -5,36 +5,41 @@
  * Ex4 - studentGithubs.forEach => O(n)
  *       Object.keys(answers).forEach => O(m) - different source => together O(mn)
  * Ex5 - O(n) => number of recepients
- *    
+ *
  */
 
+// Algorithms (JS)
 
-/*
- // Algorithms (JS)  
-  
 //Ex1
 const printStars = function (num) {
-    for (let i = 1; i <= num; i++){
-       console.log('*'.repeat(i))
+  let star = "";
+  for (let i = 0; i < num; i++) {
+    for (let x = 0; x <= i; x++) {
+      star += "*";
     }
-}
+    console.log(star);
+    star = "";
+  }
+};
 
 //Ex2
 const printBackwardsStars = function (num1) {
-    for (let j = num1; j > 0; j--){
-       console.log('*'.repeat(j))
+starBack = "";
+for (let j = num1; j > 0; j--) {
+    for (let y = j-1; y >= 0; y--) {
+        starBack += "*";
+      }
+      console.log(starBack);
+      starBack = "";
     }
-}
-
-
+};
 
 //Ex3
 const printStarSeries = function (num2, count) {
-    for (let k = 0; k < count; k++){
-        printStars(num2)
-        printBackwardsStars(num2-1)
-    }    
-}
+  for (let k = 0; k < count; k++) {
+    printStars(num2);
+    printBackwardsStars(num2 - 1);
+  }
+};
 
 printStarSeries(5, 3)
-*/
