1ac5d07f1e05c265963ce36af59c636a277eefe1
omieli <mieli.ohad@gmail.com>
2020-09-23 11:38:24 +0300

    fixed - exist() o(1) and add()

--- Data Structures/main.js
+++ Data Structures/main.js
@@ -1,25 +1,25 @@
 class UniqueArray {
-    constructor() {
-        this.uniqueStuffs = {}
-        this.length = 0
+  constructor() {
+    this.uniqueStuffs = [];
+    this.uniqeItem = {};
+    this.length = 0;
+  }
+
+  exists(item) {
+    return this.uniqueItem[item] ? true : false;
+  }
+
+  add(item) {
+      if (!this.uniqeItem[item]) {
+          this.uniqeItem[this.length] = item;
+          this.length++
     }
+  }
 
-    exists(item) { 
-        return this.uniqueStuffs[item]?true:false
-    }
+  showAll() {}
 
-    add(item) { 
-        if (!this.uniqueStuffs[item]){
-            this.uniqueStuffs[item] = true
-            this.uniqueStuffs.length++   
-        }
-    }
-    
-    showAll() { }
-
-    
-    
-    get(index) { }
-    
-
+  get(index) {}
 }
+
+const uniqueStuff = new UniqueArray();
+uniqueStuff.add("toy");
